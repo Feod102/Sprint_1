42868d8c52fe3870b3f314ddf5080f8c3cfde031
FEO <yarilowfeodor@gmail.com>
2025-05-21 17:51:00 +0300

    Добавлены решения заданий Практикума 1 спринт

---
+++ task_1.py
@@ -0,0 +1,25 @@
+time_string = '1h 45m,360s,25m,30m 120s,2h 60s'
+
+time_values = time_string.split(',')
+
+total_minutes = 0
+
+for value in time_values:
+    value = value.replace(' ', '')
+    
+    hours = 0
+    minutes = 0
+    seconds = 0
+    
+    if 'h' in value:
+        hours = int(value.split('h')[0])
+        value = value.split('h')[1]
+    if 'm' in value:
+        minutes = int(value.split('m')[0])
+        value = value.split('m')[1]
+    if 's' in value:
+        seconds = int(value.split('s')[0])
+    
+    total_minutes += hours * 60 + minutes + seconds // 60
+
+print(total_minutes)
---
+++ task_3.py
@@ -0,0 +1,18 @@
+world_champions = {
+    2002: 'Бразилия',
+    2006: 'Италия',
+    2010: 'Испания',
+    2014: 'Германия',
+    2018: 'Франция',
+}
+
+world_champions[2022] = 'Аргентина'
+for key in world_champions:
+    print(f'{key} - {world_champions[key]}')
+
+country = 'Италия'
+
+if country in world_champions:
+    print('Италия cтановилась чемпионом мира по футболу в 21 веке!')
+else:
+    print('Италия не выигрывала чемпионат мира по футболу в 21 веке.')
